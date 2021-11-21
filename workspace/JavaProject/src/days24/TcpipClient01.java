e5a4090f2d4f553feb7edb269b1ce39ab06da267
GreenShrew <90941665+GreenShrew@users.noreply.github.com>
2021-11-21 16:11:10 +0900

    weekend

--- workspace/JavaProject/src/days24/TcpipClient01.java
+++ workspace/JavaProject/src/days24/TcpipClient01.java
@@ -13,7 +13,7 @@ public class TcpipClient01 {
 		String serverIp = "192.168.0.10";
 		
 		try {
-			Socket s = new Socket(serverIp,7777);	// 내가 여는 서버. 서버에 연결하기 위해서는 위의 Ip 주소와 포트번호 7777을 적어야한다.
+			Socket s = new Socket(serverIp,7777);
 			// Socket : 클라이언트가 연결을 요청할 소프트웨어
 			// serverIp : 클라이언트가 요청할 목적지 주소
 			// 7777 : 서버에서 관리할 연결의 그룹번호
@@ -22,7 +22,7 @@ public class TcpipClient01 {
 			// 소켓에서 입력 스트림을 전달받아 입출력도구로 사용
 			InputStream in = s.getInputStream();
 			
-			// InputStream 의 단순 아스키코드 입출력을 생성자에 넣은 서버클라이언트 통신에 
+			// InputStream 의 단순 아스키코드 입출력을 생성자에 넣어 서버-클라이언트 통신에 
 			// 사용 가능한 스트림을 생성한다.
 			DataInputStream dis = new DataInputStream(in);
 			
