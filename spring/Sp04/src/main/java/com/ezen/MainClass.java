80cd440a8e7a79f4fbf1007e819d3dcba87ec448
GreenShrew <90941665+GreenShrew@users.noreply.github.com>
2022-01-28 13:59:53 +0900

    day 72
    3rd

--- spring/Sp04/src/main/java/com/ezen/MainClass.java
+++ spring/Sp04/src/main/java/com/ezen/MainClass.java
@@ -1,5 +1,7 @@
 package com.ezen;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 import com.ezen.battery.ChargeBattery;
 import com.ezen.battery.NormalBattery;
 import com.ezen.toy.ElectronicCar;
@@ -19,6 +21,18 @@ public class MainClass {
 		ChargeBattery cbatt1 = new ChargeBattery();
 		// ElectronicRadio 생성자에 필요한 전달인자 nbatt1이나 cbatt1 객체를 만들고, 이를 넣어주어야 에러가 나지 않는다.
 		ElectronicRadio radioToy1 = new ElectronicRadio(nbatt1);
+		// 생성자에 배터리 객체를 전달해주면 정상 생성. 이렇게 객체 생성시에 다른 객체의 존재 및 생성자로의 전달이 필수인 상태이다!
+		// 객체 생성이 다른 객체에 의존하고 있는 상태! 이를 객체 의존이라고 부른다.
+		// 그리고 필요 객체를 생성자의 전달인수로 넣어주는 것을 의존주입(Dependency Injection) 이라고 부른다
+		
+		
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+
+		ElectronicCar carToy2 = ctx.getBean("car", ElectronicCar.class);
+		ElectronicRadio radioToy2 = ctx.getBean("radio", ElectronicRadio.class);
+		radioToy2.setBattery(nbatt1);
+		// 스프링 프레임워크에서의 클래스 사용이 약간 업그레이드되어 사용하기 간편해졌다.
 	}
 
 }
