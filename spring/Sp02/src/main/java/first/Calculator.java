aa4896ef46aab44c77c1dda6a8029562fcf05b6d
GreenShrew <90941665+GreenShrew@users.noreply.github.com>
2022-01-28 11:29:59 +0900

    day 72
    1st Maven

---
+++ spring/Sp02/src/main/java/first/Calculator.java
@@ -0,0 +1,5 @@
+package first;
+
+public interface Calculator {
+	public int cal(int firstNum, int secondNum);	// 함수에 내용물이 없는 추상 메소드. 오버라이드 해서 쓸 예정이다.
+}
---
+++ spring/Sp02/src/main/java/first/MainClass.java
@@ -0,0 +1,22 @@
+package first;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("스프링 프레임워크 응용 소프트웨어 프로젝트입니다.");
+		
+		Calculator c = new Sum();
+		int result = c.cal(45, 78);
+		System.out.println("45+78 => " + result);
+		
+		c = new Sub();
+		result = c.cal(45, 78);
+		System.out.println("45-78 => " + result);
+		
+		c = new Mul();
+		result = c.cal(45, 78);
+		System.out.println("45*78 => " + result);
+		
+	}
+
+}
